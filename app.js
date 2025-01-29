d76acdaa3c5f9977b45821aa69c0422e219e2263
OnToanAnhDuong <thanhchuongthcsgp2@gmail.com>
2025-01-29 15:14:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -740,7 +740,7 @@ async function saveProgress() {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/vnd.github.v3+json',
-                'Authorization': 'Bearer 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'
+                'Authorization': 'Bearer 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'
             },
             body: JSON.stringify({
                 message: 'Cập nhật tiến trình học sinh',
